bba320838b83371d26b64afcef4e9a6989fa9904
Hifsjderk <hipolitDcost@protonmail.com>
2019-03-15 17:25:54 -0300

    IIIIIIIIIII Can't remembeeer

--- violent-py/1-cptr/3_zipPassCrkr/zipCrkr.py
+++ violent-py/1-cptr/3_zipPassCrkr/zipCrkr.py
@@ -1,28 +1,32 @@
 #!/usr/bin/env python3
 
 import zipfile
+import argparse
+from threading import Thread
 
 # hint to ZIP - zip evilPass.zip  secretPassFile -P "secret"
 
 def extractFile(zFile, password):
 	try:
 		zFile.extractall(pwd=str.encode(password)) #Python3: pwd: expected bytes, got str
+		print("!!! BINGO !!! => The password is " + password)
+		
 		return password
 
 	except Exception as e:
 		return 
 
 def main():
+
+	parser = argparse.ArgumentParser(description='A ZIP decypher')
+
 	zFile 		= zipfile.ZipFile("evilPass.zip")
 	passFile 	= open('dictPass.txt')
 
 	for line in passFile.readlines():
 		password 	= line.strip('\n')
-		guess 		= extractFile(zFile, password)
+		t = Thread(target=extractFile, args=(zFile, password))
+		t.start()
 
-		if guess:
-			print("!!! BINGO !!! => The password is " + password)
-			exit(0)
-		
 if __name__ == '__main__':
 	main()
